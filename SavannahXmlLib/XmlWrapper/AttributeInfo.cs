d822b7a36431edb47aefb73097849a0a23d85848
AonaSuzutsuki <rvvfake.org@gmail.com>
2020-08-12 11:28:11 +0900

    initialize.    moved XmlWapper from SavannahManager.

---
+++ SavannahXmlLib/XmlWrapper/AttributeInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace SavannahXmlLib.XmlWrapper
+{
+
+    public class AttributeInfo
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public bool IsEmpty => string.IsNullOrEmpty(Name);
+
+        public override bool Equals(object obj)
+        {
+            return obj is AttributeInfo info &&
+                   Name == info.Name &&
+                   Value == info.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -244751520;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Value);
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}=\"{Value}\"";
+        }
+    }
+}
